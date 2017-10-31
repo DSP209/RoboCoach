68239f2ad5f4497275d03842b7b5f4adf4f84fa7
David Peltzman <davidpeltzman@live.com>
2017-10-31 16:48:29 -0400

    iniit changes

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -11,9 +11,9 @@ from django.contrib.auth.views import *
 
 
 # Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
@@ -34,8 +34,8 @@ urlpatterns = [
        url(r'^logout$', logout, {  'next_page': '/'  },        name='logout')
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
